1f28de3f33261bee7f00b4819470007282385ea1
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-07-01 14:09:55 -0600

    get palindrome_less_than_n working

--- palindrome_less_than_n.rb
+++ palindrome_less_than_n.rb
@@ -7,9 +7,16 @@ class Palindrome
   def self.less_than_n(n)
     palys = []
     n.times do |num|
-      palys << num if num.to_s == num.to_s.reverse && num > 10
+      palys << num if num.to_s == reverse_string(num) && num > 10
     end
     p palys
   end
+
+  def self.reverse_string(number)
+    string_num = number.to_s
+    half_length = string_num.length / 2
+    half_length.times {|i| string_num[i], string_num[-i-1] = string_num[-i-1], string_num[i] }
+    string_num
+  end
 end
-Palindrome.less_than_n(23)
+Palindrome.less_than_n(10000)
