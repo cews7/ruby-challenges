40916d48e064c155c9ffed8175eb3ebb2c6e5a86
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-07-13 10:42:26 -0600

    finish is_alt

--- is_alt.rb
+++ is_alt.rb
@@ -1,17 +1,21 @@
 require 'pry'
 def is_alt(string)
-  characters = string.split("")
-  valid      = true
-  characters.each_with_index do |char, index|
-    if char.include?("a" || "e" || "i" || "o" || "u") && characters[index + 1].eql?("a" || "e" || "i" || "o" || "u")
+  c     = string.split("")
+  valid = true
+  c.each_with_index do |ch, i|
+    if (ch.eql?("a") || ch.eql?("e") || ch.eql?("i") || ch.eql?("o") || ch.eql?("u")) && (c[i + 1].eql?("a") || c[i + 1].eql?("e") || c[i + 1].eql?("o") ||
+      c[i + 1].eql?("i") || ch.eql?("u"))
       valid = false
-    elsif char.eql?("b" || "c" || "d" || "f" || "g" || "h" || "j" || "k" || "l" || "m" || "n" || "p" || "q" || "s" || "t" || "v") &&
-      characters[index + 1].eql?("b" || "c" || "d" || "f" || "g" || "h" || "j" || "k" || "l" || "m" || "n" || "p" || "q" || "s")
+    elsif (ch.eql?("b") || ch.eql?("c") || ch.eql?("d") || ch.eql?("f") || ch.eql?("g") || ch.eql?("h") || ch.eql?("j") || ch.eql?("k") ||
+      ch.eql?("l") || ch.eql?("m") || ch.eql?("n") || ch.eql?("p") || ch.eql?("q") || ch.eql?("s") || ch.eql?("t") || ch.eql?("v") || ch.eql?("w") ||
+      ch.eql?("x")) && (c[i + 1].eql?("b") || c[i + 1].eql?("c") || c[i + 1].eql?("d") || c[i + 1].eql?("f") || c[i + 1].eql?("g") ||
+      c[i + 1].eql?("h") || c[i + 1].eql?("j") || c[i + 1].eql?("k") || c[i + 1].eql?("l") || c[i + 1].eql?("m") || c[i + 1].eql?("n")||
+      c[i + 1].eql?("p") || c[i + 1].eql?("q") || c[i + 1].eql?("s"))
       valid = false
     end
   end
   p valid
 end
 
-is_alt("quit")
+is_alt("anime")
 # => true
