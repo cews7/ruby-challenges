5b28e34cdd17f024ccb8498072e5ed7389bec945
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-07-14 12:14:08 -0600

    extract each line into separate method

--- haiku_wizard.rb
+++ haiku_wizard.rb
@@ -14,28 +14,38 @@ class Haiku
   end
 
   def build(haiku_as_array)
-    line_one_words   = []
-    line_two_words   = []
     line_three_words = []
     first  = haiku_as_array[0]
     second = haiku_as_array[1]
     third  = haiku_as_array[2]
+    puts first_line(first), second_line(second), third_line(third)
+  end
 
+  def first_line(first)
+    line_one_words   = []
     first.each do |num|
       finder(num)
       line_one_words << words[desired_sub_arr][desired_word]
     end
+    return line_one_words.join(" ")
+  end
 
+  def second_line(second)
+    line_two_words   = []
     second.each do |num|
       finder(num)
       line_two_words << words[desired_sub_arr][desired_word]
     end
+    return line_two_words.join(" ")
+  end
 
+  def third_line(third)
+    line_three_words   = []
     third.each do |num|
       finder(num)
       line_three_words << words[desired_sub_arr][desired_word]
     end
-    puts line_one_words.join(" "), line_two_words.join(" "), line_three_words.join(" ")
+    return line_three_words.join(" ")
   end
 
   def finder(num)
