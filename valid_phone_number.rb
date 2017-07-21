9bd8d5bd695d4bab926ae33d30f24a997a9608d9
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-07-21 13:03:01 -0600

    build functional valid_phone_number_checker

--- valid_phone_number.rb
+++ valid_phone_number.rb
@@ -1,18 +1,16 @@
+require 'pry'
 def phone_number_checker(alleged_phone_number)
   m_valid = alleged_phone_number.split("").map do |char|
-    char if (0..9).member?(char.to_i) && !("a".."z").member?(char)
+    char.gsub!(/[:)!@%&"]/, "")
+    char if (0..9).member?(char.to_i) && !("a".."z").member?(char.downcase)
   end.compact
-  if m_valid.length.eql?(11) && m_valid[0].eql?("0") && !m_valid.include?(' ')
-    print m_valid.join
+  f_check = m_valid.reject { |char| char.to_s.empty? }
+  if f_check.length.eql?(11) && f_check[0].eql?("0") && !f_check.include?(' ')
+    print f_check.join
   else
     print "Not a phone number"
   end
 end
 
-
-phone_number_checker('01074834982')
+phone_number_checker('efRFS:)0207ERGQREG88349F82!')
 # => '02078834982'
-# phone_number_checker('efRFS:)0207ERGQREG88349F82!')
-# => '02078834982'
-# phone_number_checker('efRFS:)')
-# => 'Not a phone number'
