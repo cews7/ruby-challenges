72dab6e020ea0fe2bb541191dd0fdba3efd7a2f6
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-07-21 12:39:38 -0600

    add phone_number_checker

---
+++ valid_phone_number.rb
@@ -0,0 +1,19 @@
+require 'pry'
+def phone_number_checker(alleged_phone_number)
+  m_valid = alleged_phone_number.split("").map do |char|
+    char if (0..9).member?(char.to_i) && !("a".."z").member?(char)
+  end.compact
+  if m_valid.length.eql?(11) && m_valid[0].eql?("0") && !m_valid.include?(' ')
+    print m_valid.join
+  else
+    print "Not a phone number"
+  end
+end
+
+
+phone_number_checker('01074834982')
+# => '02078834982'
+# phone_number_checker('efRFS:)0207ERGQREG88349F82!')
+# => '02078834982'
+# phone_number_checker('efRFS:)')
+# => 'Not a phone number'
