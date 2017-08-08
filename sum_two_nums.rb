bf1bbd476dbaf07e2f34a71ba0fed2265b075397
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-08-07 20:06:27 -0600

    remove psuedo code

--- sum_two_nums.rb
+++ sum_two_nums.rb
@@ -1,20 +1,4 @@
 def sum_two_digits(list, sum)
-  #ASSUMPTIONS
-  #always ints?
-  #always positive?
-  #will the sum change?
-  #will there always be two nums that add to sum?
-
-  #PSUEDO CODE
-  #iterate over list looking for first possible canidatate
-  #compare said canidatate against all other ints in list
-  #check if first canidatate can find match that adds up to sum
-  #if check finds match, return each num in list (assume two ints to start)
-  #if not, move on to next first canidatate consideration
-  #repeat
-
-  #implementation
-  require 'pry'
   match_values = 0, 0
   only_nums = list.reject { |elem| elem.kind_of?(String) }
   only_nums.each_with_index do |i, outer|
@@ -25,7 +9,7 @@ def sum_two_digits(list, sum)
       end
     end
   end
-  print 'no match found' if !match_values.reduce(:+).eql? sum 
+  print 'no match found' if !match_values.reduce(:+).eql? sum
 end
 
 list_of_ints = [1, 8, "balls", 4, 5, 7]
